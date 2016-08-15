fd1614a16e6e23e6f420af173fd2446cfb8f0c32
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-08-15 14:36:21 -0600

    fix get / route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.io.emit("socketToMe", "Hello");
     res.send('respond with a resource.');
 });
 
