a7348466be1bd36722ed2891f949ecd9be73d464
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-08-16 12:29:14 -0600

    remove games.js from app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var routes = require('./routes/index');
-var Game = require('./game.js');
-var game = new Game();
+// var Game = require('./game.js');
+// var game = new Game();
 
 var app = express();
 
@@ -15,50 +15,50 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 var allGames = {
-	123: {
-		players: {
-			'/#6reys8IXaGD-L5OPAAAA': {
-				username: 'qwe',
-				score: 0,
-				currentRole: 'player'
-			},
-			'/#6reys8IXaGD-L5OPAAAA': {
-				username: 'sup',
-				score: 0,
-				currentRole: 'player'
-			}
-		}
-	}
+    123: {
+        players: {
+            '/#6reys8IXaGD-L5OPAAAA': {
+                username: 'qwe',
+                score: 0,
+                currentRole: 'player'
+            },
+            '/#6reys8IXaGD-L5OPAAAA': {
+                username: 'sup',
+                score: 0,
+                currentRole: 'player'
+            }
+        }
+    }
 };
 
 io.on("connection", socket => {
 
-	socket.on('new-game', function(data) {
-		allGames[data.gameId] = {
-			players: {
-				[socket.id]: {
-					username: data.username,
-					score: 0,
-					currentRole: 'picker'
-				}
-			}
-		}
-		socket.join(data.gameId)
-		console.log('allGames=', allGames);
-		console.log('this games players=', allGames[data.gameId].players);
-		io.emit('player-joined', data)
-	})
+    socket.on('new-game', function(data) {
+        allGames[data.gameId] = {
+            players: {
+                [socket.id]: {
+                    username: data.username,
+                    score: 0,
+                    currentRole: 'picker'
+                }
+            }
+        }
+        socket.join(data.gameId)
+        console.log('allGames=', allGames);
+        console.log('this games players=', allGames[data.gameId].players);
+        io.emit('player-joined', data)
+    })
 
-	socket.on('join-game', function(data) {
-		allGames[data.gameId].players[socket.id] = {
-			username: data.username,
-			score: 0,
-			currentRole: 'player'
-		}
-		console.log('this games players=', allGames[data.gameId].players);
-		socket.join(data.gameId)
-		io.emit('player-joined', data, allGames[data.gameId])
-	})
+    socket.on('join-game', function(data) {
+        allGames[data.gameId].players[socket.id] = {
+            username: data.username,
+            score: 0,
+            currentRole: 'player'
+        }
+        console.log('this games players=', allGames[data.gameId].players);
+        socket.join(data.gameId)
+        io.emit('player-joined', data, allGames[data.gameId])
+    })
 
 });
 
@@ -73,7 +73,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: false
+    extended: false
 }));
 app.use(cookieParser());
 
@@ -82,9 +82,9 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -92,27 +92,27 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
 module.exports = {
-	app,
-	server
+    app,
+    server
 };
--- game.js
+++ game.js
@@ -1,25 +1,25 @@
-class newGameRoom {
-  constructor () {
-    this.gameId = 0,
-    this.players = [],
-    this.round = 0
-  }
-  createGame (data) {
-    this.gameId = data.gameId,
-    this.players = [{
-      username: data.username,
-      score: 0,
-      currentRole: 'picker'
-    }]
-  }
-  joinGame (newPlayerUsername) {
-    this.players.push({
-      username: newPlayerUsername,
-      score: 0,
-      currentRole: 'player'
-    })
-  }
-  newWord () {}
-}
-
-module.exports = newGameRoom
+// class newGameRoom {
+//   constructor () {
+//     this.gameId = 0,
+//     this.players = [],
+//     this.round = 0
+//   }
+//   createGame (data) {
+//     this.gameId = data.gameId,
+//     this.players = [{
+//       username: data.username,
+//       score: 0,
+//       currentRole: 'picker'
+//     }]
+//   }
+//   joinGame (newPlayerUsername) {
+//     this.players.push({
+//       username: newPlayerUsername,
+//       score: 0,
+//       currentRole: 'player'
+//     })
+//   }
+//   newWord () {}
+// }
+//
+// module.exports = newGameRoom
