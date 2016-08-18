404e8be4b976ec1aad04b712b6227d470249a7ad
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-08-18 08:31:48 -0600

    fix room-name

--- app.js
+++ app.js
@@ -6,49 +6,47 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var routes = require('./routes/index');
-// var Game = require('./game.js');
-// var game = new Game();
 
 var app = express();
 
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-io.on("connection", socket => {
-    socket.on('join:room', function(room_name) {
-        socket.join(room_name);
+socket.on('join:room', function(data) {
+    var room_name = data.room_name;
+    socket.join(room_name);
 
-        if (io.sockets.adapter.rooms[room_name].length === 1) {
-            io.in(socket.id).emit('first_player', "first_player");
-        }
-        if (io.sockets.adapter.rooms[room_name].length === 4) {
-            io.in(room_name).emit('start_game', "starting game....");
-        }
-    });
+    if (io.sockets.adapter.rooms[room_name].length === 1) {
+        io.in(socket.id).emit('first_player', "first_player");
+    }
+    if (io.sockets.adapter.rooms[room_name].length === 4) {
+        io.in(room_name).emit('start_game', "starting game....");
+    }
+});
 
-    socket.on('select_word', function(data, room_name) {
-        io.in(room_name).emit('selected_word', data)
-    });
+socket.on('select_word', function(data, room_name) {
+    io.in(room_name).emit('selected_word', data)
+});
 
-    socket.on('leave:room', function(msg) {
-        msg.text = msg.user + ' has left the room';
-        socket.leave(msg.room);
-        socket.in(msg.room).emit('message', msg);
-    });
+socket.on('leave:room', function(msg) {
+    msg.text = msg.user + ' has left the room';
+    socket.leave(msg.room);
+    socket.in(msg.room).emit('message', msg);
+});
 
-    socket.on('send:message', function(msg) {
-        socket.in(msg.room).emit('message', msg);
-    });
+socket.on('send:message', function(msg) {
+    socket.in(msg.room).emit('message', msg);
+});
 
-    socket.on('send:definition', function(def) {
-        console.log(def);
-        io.in(def.room).emit('definition', def);
-    });
+socket.on('send:definition', function(def) {
+    console.log(def);
+    io.in(def.room).emit('definition', def);
+});
 
-    socket.on('updateChoice', function(choice, room) {
-        console.log('choice', choice)
-        io.in(room).emit('updateChoice', choice)
-    })
+socket.on('updateChoice', function(choice, room) {
+console.log('choice', choice)
+io.in(room).emit('updateChoice', choice)
+})
 });
 
 
